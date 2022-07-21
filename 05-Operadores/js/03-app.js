51105ffee7e093426d2a273e266ff923a2d8ddcb
RuddyMiranda <44390855+RuddyMiranda@users.noreply.github.com>
2022-07-21 02:08:56 -0700

    Parte 5 a 6 lista

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,17 @@
+
+// undefined
+let numero;
+
+console.log(numero);
+
+console.log(typeof numero);
+
+
+// null
+let numero2 = null;
+console.log(numero2);
+console.log(typeof numero2);
+
+// comparacion de undefined y null
+
+console.log(numero === numero2);
---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,13 @@
+const autenticado = false;
+
+// uso de if
+if(autenticado){
+    console.log('Sí, puedes ver netflix')
+}else{
+    console.log('No, no puedes verlo')
+}
+
+
+// operador ternario
+
+console.log( autenticado ? 'Si esta autenticado' : 'No esta autenticado');
